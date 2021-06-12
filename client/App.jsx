2c1d04b2225c5a47f16b5f9a0328df5d4d9f1251
GnuesJ <48038447+GnuesJ@users.noreply.github.com>
2021-06-12 18:10:03 -0400

    update package and boilerplate for frontend

--- client/App.jsx
+++ client/App.jsx
@@ -1,17 +1,57 @@
 import React, { Component } from 'react';
+import Login from './react-routers/Login.jsx';
+import User from './react-routers/User.jsx';
+import { BrowserRouter as Router, Route, Switch, Link, Redirect } from "react-router-dom";
 
-class App extends Component {
-  constructor(props){
-    super(props);
-  }
-  
-  render() {
-    return (
-      <div id='app'>
-        <h1>Scratch Project Testinggggggggg</h1>
-      </div> 
-    )
-  }
+function App() {
+  return (
+    <Router>
+
+      <Switch>
+        <Route exact path="/" component={Login}/>
+        <Route exact path="/user" component={User}/>
+      </Switch>
+
+      
+    </Router>
+  )
 }
+
+
+{/* <Router>
+      <ul className="navbar-nav mr-auto">
+            <li><Link to={'/'} className="nav-link"> Home </Link></li>
+            <li><Link to={'/Login'} className="nav-link">Contact</Link></li>
+            <li><Link to={'/User'} className="nav-link">About</Link></li>
+          </ul>
+      <Switch>
+        <Route exact path='/Login' component={Login}/>
+        <Route  path='/User' component={User}/>
+      </Switch>
+    </Router> */}
+
+/*
+
+      <Router>
+        <div>
+          <h2>Welcome to React Router Tutorial</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li><Link to={'/'} className="nav-link"> Home </Link></li>
+            <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
+            <li><Link to={'/about'} className="nav-link">About</Link></li>
+          </ul>
+          </nav>
+          <hr />
+          <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/contact' component={Contact} />
+              <Route path='/about' component={About} />
+          </Switch>
+        </div>
+      </Router>
+*/
+
+
 
 export default App;
