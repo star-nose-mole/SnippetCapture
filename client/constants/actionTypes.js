ac80593f434efdc8cb76d2b2f1cba5809b711123
GnuesJ <48038447+GnuesJ@users.noreply.github.com>
2021-06-12 21:50:51 -0400

    code snippet test

---
+++ client/constants/actionTypes.js
@@ -0,0 +1,25 @@
+
+export const INIT_USER = 'INIT_USER';
+export const ADD_BUYPOWER = 'ADD_BUYPOWER';
+export const BUY_CRYPTO = 'BUY_CRYPTO';
+export const SELL_CRYPTO = 'SELL_CRYPTO';
+export const UPDATE_VIRTUALMONEY = 'UPDATE_VIRTUALMONEY'
+
+
+
+const state = {
+  tags: {
+    tagID1: tagName
+  },
+  snippets: [
+    {
+      snippetCode: 'for let people count',
+      tags: [
+        tagName,
+      ]
+    }
+  ]
+}
+
+
+
--- client/react-routers/User.jsx
+++ client/react-routers/User.jsx
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 
 
 const User = props => {
+  const [code, setCode] = React.useState('');
 
+  const codeHandler = (event) => {
+    setCode(event.target.value);
+  }
   return(
     <div>
       You made it!
+      <pre>
+      <code>
+        {code}
+      </code>
+      </pre>
+      <textarea onChange={codeHandler} cols='50' rows='10'></textarea>
     </div>
   )
 
