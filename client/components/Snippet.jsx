b7e27094a00c4532eb259754b6cab3c045ae9808
Matthew <matthewlai93@gmail.com>
2021-06-14 12:17:46 -0400

    added SnippetContainer, Snippet component, and Tag component

--- client/components/Snippet.jsx
+++ client/components/Snippet.jsx
@@ -1,12 +1,23 @@
 import React, { Component, useEffect, useState } from 'react';
-
+import Tag from './Tag.jsx'
 
 
 
 const Snippet = props => {
+  const tagsArr = [];
+
+  for (let i = 0; i < props.tags.length; i++) {
+    tagsArr.push(<Tag name={props.tags[i]} />)
+  }
+  // <div1> {props.tags.join(', ')}</div1>
+
   return(
-    <div>
-      Hello There!
+    <div id='snippet'>
+     <textarea>{props.code}</textarea>
+     <a href={props.URL} rel="noreferrer">
+          Find the code here!
+        </a>
+      {tagsArr}
     </div>
   )
 }
