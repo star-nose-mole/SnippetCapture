ec5433b66873bba8215c7126ccf03c86849598ba
GnuesJ <48038447+GnuesJ@users.noreply.github.com>
2021-06-13 23:19:20 -0400

    work on search.jsx

--- client/components/Search.jsx
+++ client/components/Search.jsx
@@ -1,11 +1,60 @@
 import React, { Component, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import * as actions from '../actions/actions.js'
+import regeneratorRuntime from "regenerator-runtime";
 
 const SearchReact = (props) => {
+  const dispatch = useDispatch();
+  let searchTags = useSelector(state => state.reducer.search);
+
+  const [searchSentence, setSearchSentence] = useState('');
+
+  const updatesearchHandler =  (event) => {
+    // console.log(event.target.value);
+    // setSearchSentence(event.target.value);
+    dispatch(actions.updateSearchCreator(event.target.value));
+    console.log(searchTags);
+
+  }
+
+  const searchEnterHandler = (event) => {
+    if(event.key === 'Enter'){
+      console.log('Enter was pressed, doing fetch get request');
+      console.log(searchTags);
+      // fetch('/api/search', {
+      //   headers: {'Accept': 'application/json, text/plain, */*',
+      //     'Content-Type': 'application/json'},
+      //   body: JSON.stringify({tags: searchTags})
+      // })
+
+      //this is for fetching init
+      // fetch('/api/search', {
+      //   headers: {'Accept': 'application/json, text/plain, */*',
+      //     'Content-Type': 'application/json'},
+      //   body: JSON.stringify({tags: []})
+      // })
+
+      //this is for adding snippet
+      // fetch('/api/addsnippet', {
+      //   method: 'POST',
+      //   headers: {'Accept': 'application/json, text/plain, */*',
+      //     'Content-Type': 'application/json'},
+      //   body: JSON.stringify(
+      //     {
+      //       snippetCode: '',
+      //       url: '',
+      //       tags: [],
+      //     }
+      //   )
+      // })
+    }
+    // console.log(search);
+      
+  }
+
   return (
     <div>
-      searching component
+      <input type='text' onKeyDown={searchEnterHandler} onChange={updatesearchHandler}></input>
     </div>
   )
 }
